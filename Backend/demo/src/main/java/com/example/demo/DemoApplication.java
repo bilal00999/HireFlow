0e1447c29cba6ed174a7afc1743c4cd4d2c1658f
bilal00999 <bilalahmed20051@gmail.com>
2026-08-06 15:23:20 +0500

    update packages

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println(System.getenv("DB_URL"));
+		System.out.println(System.getenv("DB_USERNAME"));
 	}
 
 }
